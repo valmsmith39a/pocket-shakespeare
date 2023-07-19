71b5778cdf5b5d6e85b4cf50a4ce818fbb82d3c8
valmsmith39a <valmsmith39a@gmail.com>
2023-07-19 03:45:27 +0000

    Begin preprocessing data - get unique chars and sort

--- preprocessing.py
+++ preprocessing.py
@@ -6,8 +6,14 @@ class Preprocessor:
 		url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
 		response = requests.get(url)
 		tiny_shakespeare_text = response.text
+		self.clean_data(tiny_shakespeare_text[:100])
 		return tiny_shakespeare_text[:100]
 		
-	def clean_data(self):
-		pass
+	def clean_data(self, text):
+		# set: gets the unique chars from text 
+		# list: puts chars into a list 
+		chars = sorted(list(set(text)))
+		vocab_size = len(chars)
+		print("chars: ", chars)
+		print("vocab size: ", vocab_size)
 		

68adb7eb5c156a3e6864fea3279d64cedc988120
valmsmith39a <valmsmith39a@gmail.com>
2023-07-21 00:37:41 +0000

    Integer encoding / decoding

--- preprocessing.py
+++ preprocessing.py
@@ -6,14 +6,23 @@ class Preprocessor:
 		url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
 		response = requests.get(url)
 		tiny_shakespeare_text = response.text
-		self.clean_data(tiny_shakespeare_text[:100])
+		self.clean_data(tiny_shakespeare_text)
 		return tiny_shakespeare_text[:100]
 		
 	def clean_data(self, text):
-		# set: gets the unique chars from text 
-		# list: puts chars into a list 
+		"""
+  		Character level tokenization 
+		set: extracts unique chars from text 
+  		list: puts chars into a list 
+  		"""
 		chars = sorted(list(set(text)))
 		vocab_size = len(chars)
-		print("chars: ", chars)
-		print("vocab size: ", vocab_size)
+		stoi = { ch:i for i,ch in enumerate(chars) } # string to index, mapping from chars to integers
+		itos = { i:ch for i,ch in enumerate(chars) } # index to string, mapping from integers to chars 
+		# encoder: take a string, output a list of integers 
+	    # Ex "hii there" -> [46, 47, 47, 1, 58, 46, 43, 56, 43]	
+		encode = lambda s: [ stoi[c] for c in s ] 
+		# decoder: take a list of integers and output a string
+		# Ex [46, 47, 47, 1, 58, 46, 43, 56, 43] -> "hii there"	
+		decode = lambda l: ''.join([itos[i] for i in l])
 		
